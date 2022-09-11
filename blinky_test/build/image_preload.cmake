d6a8fd2b359c8b7052158273e785631206a43e53
Hemjal <abuhemjalharun@yahoo.com>
2022-09-11 16:39:13 +0300

    first blinky project

---
+++ blinky_test/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/nrf5340dk/blinky_test/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
+set(CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "c:\ncs\toolchains\v2.0.2\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
